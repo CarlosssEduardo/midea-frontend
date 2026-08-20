3b3d8b3f274811b87fdfb29830ae25bdbafec9c2
Carlos Eduardo <carloseduardof191@gmail.com>
2026-08-20 17:05:07 -0300

    Removendo configuracao de esbuild do vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ou o plugin que você estiver usando (vue, etc.)
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    transformer: 'postcss', // Força o Vite a usar o PostCSS para ler o Tailwind
-  },
-  build: {
-    cssMinify: 'esbuild', // Usa o esbuild para minificar o CSS sem conflitos
-  }
 })
